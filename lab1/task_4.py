7d674221f9e956bdf4dda72a7ace748829a41f35
vvernovskaya <55106538+vvernovskaya@users.noreply.github.com>
2019-09-10 19:40:46 +0300

    Update task_4.py

--- lab1/task_4.py
+++ lab1/task_4.py
@@ -5,16 +5,15 @@ from pyrob.api import *
 
 @task
 def task_3_3():
-	if wall_is_on_the_right() and wall_is_on_the_left() and wall_is_above():
-		move_down()
-	if wall_is_on_the_right() and wall_is_on_the_left() and wall_is_beneath():
+	if not wall_is_above():
 		move_up()
-	if wall_is_on_the_right() and wall_is_beneath() and wall_is_above():
-		move_left()
-	if wall_is_on_the_left() and wall_is_on_the_left() and wall_is_above():
+	if not wall_is_beneath():
+		move_down()
+	if not wall_is_on_the_right():
 		move_right()
+	if not wall_is_on_the_left():
+		move_left()
 		
-
 
 if __name__ == '__main__':
 	run_tasks()
