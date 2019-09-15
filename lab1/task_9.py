92a06b79ce5855866b6a2fd25516ad238f6d1a6e
vvernovskaya <55106538+vvernovskaya@users.noreply.github.com>
2019-09-15 14:00:16 +0300

    Update task_9.py

--- lab1/task_9.py
+++ lab1/task_9.py
@@ -5,11 +5,9 @@ from pyrob.api import *
 
 @task
 def task_8_2():
-	while wall_is_beneath() or wall_is_above():
-		if (wall_is_beneath() and not wall_is_above()) or (wall_is_above() and not wall_is_beneath()):
+	while not wall_is_on_the_right():
+		if not wall_is_above():
 			fill_cell()
-		if wall_is_on_the_right():
-			break
 		move_right()
 
 
