afa7a19a395085db45da12d0ba578166535ff593
vvernovskaya <55106538+vvernovskaya@users.noreply.github.com>
2019-09-15 20:17:09 +0300

    Update task12.py

--- lab2/task12.py
+++ lab2/task12.py
@@ -3,24 +3,17 @@ import turtle
 t = turtle.Turtle()
 t.shape('turtle')
 
-def circle(i):
-	for k in range(i):
-		t.forward(1)
-		t.right(180 - 180*(i-2)/i)
-	for k in range(i):
-		t.forward(1)
-		t.left(180 - 180*(i-2)/i)
-	
+
 t.left(90)
 
 def semicircle(i):
-	for k in range(i/2):
+	for k in range(int(i/2)):
 		t.forward(1)
-		t.right(180 - 180*(i-2)/i)
+		t.right(360/i)
 
 for i in range(10):
 	semicircle(200)
-	semicircle(30)
+	semicircle(50)
 	
 
 	
