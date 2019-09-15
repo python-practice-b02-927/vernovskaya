bf3f4c5939e1a64849c4ef6af8aaddbbd779142c
vvernovskaya <55106538+vvernovskaya@users.noreply.github.com>
2019-09-15 19:55:48 +0300

    Update task9.py

--- lab2/task9.py
+++ lab2/task9.py
@@ -1,23 +1,27 @@
 import turtle
+import math
+
 
 t = turtle.Turtle()
 t.shape('turtle')
 
-def angle(i):
-	for k in range(i):
-		t.forward(20 + i*10)
-		t.right(180 - 180*(i-2)/i)
-	t.penup()
-	t.left(90)
-	t.forward(10)
-	t.right(90)
-	t.pendown()
-	
-	
 
+
+def angle(i,r):
+    t.left(90 + 180/i)
+    for k in range(i):
+        t.forward(2*r*math.sin(math.pi/i))
+        t.left(360/i)
+    t.right(90 + 180/i)
+    t.penup()
+    t.forward(20)
+    t.pendown()
+    
+
+r = 20    
 for i in range(3, 13):
-	angle(i)
-
-	
+    angle(i,r)
+    r = r + 20
+    print(r)
 	
 input()
