9643caad879cab99f4f40ff0f70f1e8b819ea4f9
vvernovskaya <vvernovskaya@gmail.com>
2019-09-20 23:01:29 +0300

    task_22 edited due to the issue

--- lab1/task_22.py
+++ lab1/task_22.py
@@ -2,20 +2,16 @@
 
 from pyrob.api import *
 
+def right_wall(m):
+    fill_cell()
+    move_right(n=m)
+      
 
 @task (delay = 0.01)
 def task_5_10():
     k = 0
     m = 0
-    if wall_is_beneath():
-        if wall_is_on_the_right():
-            fill_cell()
-            return
-        while not wall_is_beneath():
-            fill_cell()
-            move_down()
-        fill_cell()
-        return
+    
     while not wall_is_beneath():
         move_down()
         k = k + 1
@@ -24,26 +20,16 @@ def task_5_10():
         m = m + 1
     while not wall_is_above():
         move_up()
-    while not wall_is_on_the_left():
-        move_left()
     
-
-    for j in range(k+1):
-        if wall_is_on_the_left():
+    for i in range(k+1):
+        for b in range(m):
             fill_cell()
-            for i in range(m):
-                move_right()
-                fill_cell()
-        elif wall_is_on_the_right():
+            move_left()   
+        if i == k:
             fill_cell()
-            for i in range(m):
-                move_left()
-                fill_cell()
-        if j != k:
-            move_down()
-    if wall_is_on_the_right():
-        move_left(n=m)
-        
+            break
+        right_wall(m)
+        move_down()
 
 
 if __name__ == '__main__':
