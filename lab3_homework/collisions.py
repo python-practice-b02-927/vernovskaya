694b0bcea6f94835f048c69532905b4b8a5156a3
vvernovskaya <vvernovskaya@gmail.com>
2019-09-27 22:39:04 +0300

    make coords1 and coords2 for both blocks, input m1 and m2, edit draw_block1() and draw_block2()

--- lab3_homework/collisions.py
+++ lab3_homework/collisions.py
@@ -5,26 +5,28 @@ SIZE_Y = 400
 
 w = gr.GraphWin("Collisions", SIZE_X, SIZE_Y)
 
-coords = gr.Point(200, 200)
+coords1 = gr.Point(200, 200)
+coords2 = gr.Point(300, 200)
 
 velocity = gr.Point(-1, 0)
 
+
 def add(point_1, point_2):
-    new_point = Point(point_1.x + point_2.x,
+    new_point = gr.Point(point_1.x + point_2.x,
                       point_1.y + point_2.y)
 
     return new_point
 
 
-def draw_block1(coords):
-    block1 = gr.Rectangle(coords, coords )
-    block2.setFill('cyan')
+def draw_block1(coords, w):
+    block1 = gr.Rectangle(coords1, add(coords1, gr.Point(40,20)))
+    block1.setFill('cyan')
     
     block1.draw(w)
 
 
-def draw_block2(coords):
-    block2 = gr.Rectangle(coords, coords )
+def draw_block2(coords, w):
+    block2 = gr.Rectangle(coords2, add(coords2, gr.Point(40,20)))
     block2.setFill('blue')
     
     block2.draw(w)
@@ -43,7 +45,13 @@ def check_other_block():
 
 
 def main(w):
-    pass
+    coords = gr.Point(200, 200)
+    m1 = int(input('Enter m1: '))
+    m2 = int(input('Enter m2: '))
+    draw_block1(coords, w)
+    draw_block2(coords, w)
+
+    
 
 
 main(w)
