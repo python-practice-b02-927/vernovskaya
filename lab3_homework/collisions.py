0df27b69c624538b464a486b587933d7b842a6dc
vvernovskaya <vvernovskaya@gmail.com>
2019-09-27 21:32:19 +0300

    def check_wall(), def check_other_block(), def main()

--- lab3_homework/collisions.py
+++ lab3_homework/collisions.py
@@ -3,7 +3,7 @@ import graphics as gr
 SIZE_X = 800
 SIZE_Y = 400
 
-window = gr.GraphWin("Collisions", SIZE_X, SIZE_Y)
+w = gr.GraphWin("Collisions", SIZE_X, SIZE_Y)
 
 coords = gr.Point(200, 200)
 
@@ -18,4 +18,15 @@ def draw_block():
 def clear_window():
     pass
 
+def check_wall():
+    pass
 
+def check_other_block():
+    pass
+
+def main():
+    pass
+
+main(w)
+w.getMouse()
+w.close()
