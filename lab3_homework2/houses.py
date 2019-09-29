e70511295c0b94ff7037a964aa10cf33f5140621
vvernovskaya <vvernovskaya@gmail.com>
2019-09-29 13:53:05 +0300

    describe tree function, change coordinates

--- lab3_homework2/houses.py
+++ lab3_homework2/houses.py
@@ -28,9 +28,11 @@ def house(w, h, p): #p stands for starting point, h stands for height
     body = gr.Rectangle(p, gr.Point(p.x + (4 / 3)*h, p.y + h))
     body.setFill('chocolate')
     body.draw(w)
+    
     window = gr.Rectangle(gr.Point((p.x + (4 / 6) * h - h / 5), p.y + h / 3), gr.Point((p.x + (4 / 6) * h + h / 5), p.y + h*(2 / 3)))
     window.setFill('cyan3')
     window.draw(w)
+    
     roof = gr.Polygon(p, gr.Point(p.x + (4 / 3) * h, p.y), gr.Point(p.x + (4 / 6) * h, p.y - h / 2))
     roof.setFill('red')
     roof.draw(w)
@@ -38,17 +40,33 @@ def house(w, h, p): #p stands for starting point, h stands for height
 
 def cloud(w, r, p): 
     for i in range(4):
-        cloud1 = gr.Circle(gr.Point(p.x + i*(r + 5), p.y), r)
+        cloud1 = gr.Circle(gr.Point(p.x + i * (r + 5), p.y), r)
         cloud1.setFill('white')
         cloud1.draw(w)
+        
     for i in range(2):
-        cloud1 = gr.Circle(gr.Point(p.x + r*(7 / 3) - i*(r + 5), p.y - r), r)
+        cloud1 = gr.Circle(gr.Point(p.x + r * (7 / 3) - i * (r + 5), p.y - r), r)
         cloud1.setFill('white')
         cloud1.draw(w)
 
 
 def tree(w, r, p):
-    leaves = gr.Circle(gr.Point(p.x - (r + 5), p.y), r)
+    stick = gr.Rectangle(gr.Point(p.x - r / 6, p.y + 2 * r), gr.Point(p.x + r / 6, p.y + 5 * r))
+    stick.setFill('black')
+    stick.draw(w)
+    
+    for i in range(1, 3):
+        leaves1 = gr.Circle(gr.Point(p.x, p.y + (i - 1) * r), r)
+        leaves1.setFill('green4')
+        leaves1.draw(w)
+        
+        leaves2 = gr.Circle(gr.Point(p.x - r + i, p.y + i * r), r)
+        leaves2.setFill('green4')
+        leaves2.draw(w)
+        
+        leaves3 = gr.Circle(gr.Point(p.x + r + i, p.y + i * r), r)
+        leaves3.setFill('green4')
+        leaves3.draw(w)
 
 
 def main(w):
@@ -56,12 +74,12 @@ def main(w):
     grass(w)
     sun(w)
     house(w, 200, gr.Point(100, 420))
-    house(w, 120, gr.Point(560, 410))
-    cloud(w, 30, gr.Point(250, 200))
-    cloud(w, 30, gr.Point(700, 200))
-    cloud(w, 20, gr.Point(500, 250))
-    tree(w, 40, gr.Point(500, 300))
-    tree(w, 30, gr.Point(500, 300))
+    house(w, 120, gr.Point(590, 410))
+    cloud(w, 30, gr.Point(250, 180))
+    cloud(w, 30, gr.Point(700, 180))
+    cloud(w, 20, gr.Point(500, 220))
+    tree(w, 40, gr.Point(470, 300))
+    tree(w, 30, gr.Point(800, 300))
 
 
 main(w)
