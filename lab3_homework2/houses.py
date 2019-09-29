e45a03a17d4082a46418f84fa9a9a91eabd3058d
vvernovskaya <vvernovskaya@gmail.com>
2019-09-29 13:28:06 +0300

    edit tree function, change coordinates

--- lab3_homework2/houses.py
+++ lab3_homework2/houses.py
@@ -25,9 +25,15 @@ def sun(w):
 
 
 def house(w, h, p): #p stands for starting point, h stands for height
-    body = gr.Rectangle(p, gr.Point(p.x + (4/3)*h, p.y + h))
+    body = gr.Rectangle(p, gr.Point(p.x + (4 / 3)*h, p.y + h))
     body.setFill('chocolate')
     body.draw(w)
+    window = gr.Rectangle(gr.Point((p.x + (4 / 6) * h - h / 5), p.y + h / 3), gr.Point((p.x + (4 / 6) * h + h / 5), p.y + h*(2 / 3)))
+    window.setFill('cyan3')
+    window.draw(w)
+    roof = gr.Polygon(p, gr.Point(p.x + (4 / 3) * h, p.y), gr.Point(p.x + (4 / 6) * h, p.y - h / 2))
+    roof.setFill('red')
+    roof.draw(w)
 
 
 def cloud(w, r, p): 
@@ -36,13 +42,13 @@ def cloud(w, r, p):
         cloud1.setFill('white')
         cloud1.draw(w)
     for i in range(2):
-        cloud1 = gr.Circle(gr.Point(p.x + r*(7/3) - i*(r + 5), p.y - r), r)
+        cloud1 = gr.Circle(gr.Point(p.x + r*(7 / 3) - i*(r + 5), p.y - r), r)
         cloud1.setFill('white')
         cloud1.draw(w)
 
 
-def tree(w, r):
-    pass
+def tree(w, r, p):
+    leaves = gr.Circle(gr.Point(p.x - (r + 5), p.y), r)
 
 
 def main(w):
@@ -51,11 +57,11 @@ def main(w):
     sun(w)
     house(w, 200, gr.Point(100, 420))
     house(w, 120, gr.Point(560, 410))
-    cloud(w, 30, gr.Point(250, 250))
-    cloud(w, 30, gr.Point(700, 250))
-    cloud(w, 20, gr.Point(500, 300))
-    tree(w, 40)
-    tree(w, 30)
+    cloud(w, 30, gr.Point(250, 200))
+    cloud(w, 30, gr.Point(700, 200))
+    cloud(w, 20, gr.Point(500, 250))
+    tree(w, 40, gr.Point(500, 300))
+    tree(w, 30, gr.Point(500, 300))
 
 
 main(w)
