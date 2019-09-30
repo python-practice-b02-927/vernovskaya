d44f2acdd893cf379bc7a54db7b32898098e168d
vvernovskaya <vvernovskaya@gmail.com>
2019-10-01 00:53:56 +0300

    minor changes due to the issue

--- lab3_homework2/houses.py
+++ lab3_homework2/houses.py
@@ -28,13 +28,15 @@ def house(w, h, p): #p stands for starting point, h stands for height
     body = gr.Rectangle(p, gr.Point(p.x + (4 / 3)*h, p.y + h))
     body.setFill('chocolate')
     body.draw(w)
-    
-    window = gr.Rectangle(gr.Point((p.x + (4 / 6) * h - h / 5), p.y + h / 3), gr.Point((p.x + (4 / 6) * h + h / 5), p.y + h*(2 / 3)))
+
+    window = gr.Rectangle(gr.Point((p.x + (4 / 6) * h - h / 5), p.y + h / 3), \
+                          gr.Point((p.x + (4 / 6) * h + h / 5), p.y + h*(2 / 3)))
     window.setFill('cyan3')
     window.setOutline('orange')
     window.draw(w)
-    
-    roof = gr.Polygon(p, gr.Point(p.x + (4 / 3) * h, p.y), gr.Point(p.x + (4 / 6) * h, p.y - h / 2))
+
+    roof = gr.Polygon(p, gr.Point(p.x + (4 / 3) * h, p.y), \
+                      gr.Point(p.x + (4 / 6) * h, p.y - h / 2))
     roof.setFill('red')
     roof.draw(w)
 
@@ -44,7 +46,7 @@ def cloud(w, r, p):
         cloud1 = gr.Circle(gr.Point(p.x + i * (r + 5), p.y), r)
         cloud1.setFill('white')
         cloud1.draw(w)
-        
+
     for i in range(2):
         cloud1 = gr.Circle(gr.Point(p.x + r * (7 / 3) - i * (r + 5), p.y - r), r)
         cloud1.setFill('white')
@@ -52,19 +54,20 @@ def cloud(w, r, p):
 
 
 def tree(w, r, p):
-    stick = gr.Rectangle(gr.Point(p.x - r / 6, p.y + 2 * r), gr.Point(p.x + r / 6, p.y + 5 * r))
+    stick = gr.Rectangle(gr.Point(p.x - r / 6, p.y + 2 * r), \
+                         gr.Point(p.x + r / 6, p.y + 5 * r))
     stick.setFill('black')
     stick.draw(w)
-    
+
     for i in range(1, 3):
         leaves1 = gr.Circle(gr.Point(p.x, p.y + (i - 1) * r), r)
         leaves1.setFill('green4')
         leaves1.draw(w)
-        
+
         leaves2 = gr.Circle(gr.Point(p.x - r + i, p.y + i * r), r)
         leaves2.setFill('green4')
         leaves2.draw(w)
-        
+
         leaves3 = gr.Circle(gr.Point(p.x + r + i, p.y + i * r), r)
         leaves3.setFill('green4')
         leaves3.draw(w)
