98b51e83e176c92681d9ec06c5c864781e506c54
vvernovskaya <vvernovskaya@gmail.com>
2019-10-15 01:35:46 +0300

    add leaderboard and edit due to pep8

--- lab4/catchtheball.py
+++ lab4/catchtheball.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from random import randrange as rnd, choice
 import time
+import sys
+import json
 
 root = Tk()
 root.geometry('800x600')
@@ -14,14 +16,19 @@ l = Label(root, bg='black', fg='white', width=20)
 l.pack()
 
 count = 0
+i = 0
 
 d = {}
+lead = {}
 n = 0
+
+name = input('Enter your name: ')
 
 
 def move():
     global x, y, r, i, ballcolor, vx, vy
     canv.delete(ALL)
+
     for id_ in d:
         if d[id_]['x'] > 800 or d[id_]['x'] < 0:
             d[id_]['vx'] = (-1)*d[id_]['vx']
@@ -30,6 +37,7 @@ def move():
         d[id_]['x'] += d[id_]['vx']
         d[id_]['y'] += d[id_]['vy']
         id_ = canv.create_oval(d[id_]['x']-d[id_]['r'],d[id_]['y']-d[id_]['r'],d[id_]['x']+d[id_]['r'],d[id_]['y']+d[id_]['r'],fill = d[id_]['color'], width=0)
+
     root.after(200, move)
 
 
@@ -41,8 +49,10 @@ def special_ball():
     vx = 12
     vy = 12
     r = 100
+
     id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = 'maroon', width=0) 
     d[id_] = {'x': x, 'y': y, 'r': r, 'vx': vx, 'vy': vy, 'color': 'maroon'}
+
     move()
 
 
@@ -54,14 +64,26 @@ def new_ball():
     vx = rnd(-7,7)
     vy = rnd(-7,7)
     ballcolor = choice(colors)
+    
     id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = ballcolor, width=0) 
     d[id_] = {'x': x, 'y': y, 'r': r, 'vx': vx, 'vy': vy, 'color': ballcolor}
+
     move()
+
+    i += 1
+    if int(i) == 15:
+        root.destroy()
+        lead[name] = {'name': name, 'score': count}
+        with open('leaderboard.json', 'a') as f:
+            json.dump(lead[name], f, indent=2)
+        sys.exit(0)
+
     root.after(3000, new_ball)
 
 
 def click(event):
     global count
+
     for id_ in d:
         extra = 0
         if (event.x - d[id_]['x'])**2 + (event.y - d[id_]['y'])**2 <= d[id_]['r']**2:
@@ -69,15 +91,19 @@ def click(event):
                 count += 10
             else:
                 count += 1
-            l['text'] = count
             extra = id_
             canv.delete(id_)
             break
+
     if extra != 0:
-        del d[extra]
+       del d[extra]
+
+    l['text'] = count
     new_ball()
 
 
 new_ball()
 root.after(5000, special_ball)
+
 canv.bind('<Button-1>', click)
+root.mainloop()
