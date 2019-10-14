894ae6f8102f8bb491995c66833fdbd9da4a43de
vvernovskaya <vvernovskaya@gmail.com>
2019-10-14 17:35:13 +0300

    add special_ball() and minor changes

--- lab4/catchtheball.py
+++ lab4/catchtheball.py
@@ -14,7 +14,6 @@ l = Label(root, bg='black', fg='white', width=20)
 l.pack()
 
 count = 0
-extra = 0
 
 d = {}
 n = 0
@@ -31,7 +30,20 @@ def move():
         d[id_]['x'] += d[id_]['vx']
         d[id_]['y'] += d[id_]['vy']
         id_ = canv.create_oval(d[id_]['x']-d[id_]['r'],d[id_]['y']-d[id_]['r'],d[id_]['x']+d[id_]['r'],d[id_]['y']+d[id_]['r'],fill = d[id_]['color'], width=0)
-    root.after(300, move)
+    root.after(200, move)
+
+
+def special_ball():
+    global x, y, r, i, ballcolor, vx, vy
+    x = rnd(100,700)
+    y = rnd(100,500)
+    r = rnd(30,50)
+    vx = 12
+    vy = 12
+    r = 100
+    id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = 'maroon', width=0) 
+    d[id_] = {'x': x, 'y': y, 'r': r, 'vx': vx, 'vy': vy, 'color': 'maroon'}
+    move()
 
 
 def new_ball():
@@ -45,14 +57,18 @@ def new_ball():
     id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = ballcolor, width=0) 
     d[id_] = {'x': x, 'y': y, 'r': r, 'vx': vx, 'vy': vy, 'color': ballcolor}
     move()
-    root.after(4000, new_ball)
+    root.after(3000, new_ball)
 
 
 def click(event):
-    global count, extra
+    global count
     for id_ in d:
+        extra = 0
         if (event.x - d[id_]['x'])**2 + (event.y - d[id_]['y'])**2 <= d[id_]['r']**2:
-            count += 1
+            if d[id_]['color'] == 'maroon':
+                count += 10
+            else:
+                count += 1
             l['text'] = count
             extra = id_
             canv.delete(id_)
@@ -63,4 +79,5 @@ def click(event):
 
 
 new_ball()
+root.after(5000, special_ball)
 canv.bind('<Button-1>', click)
