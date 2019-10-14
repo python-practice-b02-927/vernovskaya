e5f1e20b6be5e60f37be0be6b912ed868ff36963
vvernovskaya <vvernovskaya@gmail.com>
2019-10-14 16:22:05 +0300

    move ball

--- lab4/catchtheball.py
+++ lab4/catchtheball.py
@@ -18,28 +18,44 @@ count = 0
 d = {}
 n = 0
 
-def move():
-    global x, y, ballcolor
-    x = x + 5
-    y = y + 5
-    canv.delete(ALL)
-    id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = ballcolor, width=0)
-    d[id_] = {'x': x, 'y': y, 'r': r}
-    for id_ in d:
-        id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = ballcolor, width=0)
-        d[id_] = {'x': x, 'y': y, 'r': r}
-    root.after(200, move)
-
-
-def new_ball():
-    global x, y, r, i, ballcolor
+def first_ball():
     x = rnd(100,700)
     y = rnd(100,500)
     r = rnd(30,50)
     ballcolor = choice(colors)
+    id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = ballcolor, width=0)
+    d[id_] = {'x': x, 'y': y, 'r': r}
     
-    root.after(2000, move)
-    root.after(2000, new_ball)
+
+def move():
+    global x, y, r, i, ballcolor, vx, vy
+    for id_ in d:
+        del d[id_]
+        x = x + vx
+        y = y + vy
+        if x > 800 or x < 0:
+            vx = -vx
+        if y > 600 or y < 0:
+            vy = -vy
+        canv.delete(id_)
+        id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = ballcolor, width=0)
+        d[id_] = {'x': x, 'y': y, 'r': r, 'vx': vx, 'vy': vy}
+    root.after(200, move)
+
+
+def new_ball():
+    global x, y, r, i, ballcolor, vx, vy
+    x = rnd(100,700)
+    y = rnd(100,500)
+    r = rnd(30,50)
+    vx = rnd(-5,5)
+    vy = rnd(-5,5)
+    ballcolor = choice(colors)
+    id_ = canv.create_oval(x-r,y-r,x+r,y+r,fill = ballcolor, width=0) 
+    d[id_] = {'x': x, 'y': y, 'r': r, 'vx': vx, 'vy': vy}
+    move()
+    root.after(4000, new_ball)
+    
     
 
 def click(event):
@@ -48,10 +64,10 @@ def click(event):
         if (event.x - d[id_]['x'])**2 + (event.y - d[id_]['y'])**2 <= d[id_]['r']**2:
             count += 1
             l['text'] = count
-            elem = d.pop(id_) # вынести удаление за цикл
-            canv.delete(id_)
-            new_ball()
+            break
+    canv.delete(id_)
+    new_ball()
 
-
+first_ball()
 new_ball()
 canv.bind('<Button-1>', click)
