5e8d142d04876d1eda393e67ae3d707387d1896c
vvernovskaya <vvernovskaya@gmail.com>
2019-10-22 16:03:23 +0300

    edit due to pep8

--- lab8/gun.py
+++ lab8/gun.py
@@ -12,7 +12,7 @@ canv = tk.Canvas(root, bg='white')
 canv.pack(fill=tk.BOTH, expand=1)
 
 
-class ball():
+class Ball():
     def __init__(self, x=40, y=450):
         """ Конструктор класса ball
 
@@ -27,21 +27,21 @@ class ball():
         self.vy = 0
         self.color = choice(['blue', 'green', 'red', 'brown'])
         self.id = canv.create_oval(
-                self.x - self.r,
-                self.y - self.r,
-                self.x + self.r,
-                self.y + self.r,
-                fill=self.color
+            self.x - self.r,
+            self.y - self.r,
+            self.x + self.r,
+            self.y + self.r,
+            fill=self.color
         )
         self.live = 10
 
     def set_coords(self):
         canv.coords(
-                self.id,
-                self.x - self.r,
-                self.y - self.r,
-                self.x + self.r,
-                self.y + self.r
+            self.id,
+            self.x - self.r,
+            self.y - self.r,
+            self.x + self.r,
+            self.y + self.r
         )
 
     def move(self):
@@ -52,22 +52,23 @@ class ball():
         и стен по краям окна (размер окна 800х600).
         """
         if self.y > 550 or self.y < 0:
-            self.vy = (-0.5)*self.vy
-            self.vx = (0.7)*self.vx
+            self.vy = (-0.5) * self.vy
+            self.vx = (0.7) * self.vx
             self.live -= 1
+
         if self.x < 0 or self.x > 800:
             if self.x < 0:
                 self.x = 1
+
             if self.x > 800:
                 self.x = 799
             self.vx = -self.vx
             self.live -= 1
-        self.x += self.vx 
+
+        self.x += self.vx
         self.y -= self.vy
         self.vy -= 2
         self.set_coords()
-        
-
 
     def hittest(self, obj):
         """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
@@ -81,15 +82,14 @@ class ball():
             return True
         else:
             return False
-        
 
 
-class gun():
+class Gun():
     def __init__(self):
         self.f2_power = 10
         self.f2_on = 0
         self.an = 1
-        self.id = canv.create_line(20,450,50,420,width=7) 
+        self.id = canv.create_line(20, 450, 50, 420, width=7)
 
     def fire2_start(self, event):
         self.f2_on = 1
@@ -101,10 +101,11 @@ class gun():
         Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
         """
         global balls, bullet
+
         bullet += 1
-        new_ball = ball()
+        new_ball = Ball()
         new_ball.r += 5
-        self.an = math.atan((event.y-new_ball.y) / (event.x-new_ball.x))
+        self.an = math.atan((event.y - new_ball.y) / (event.x - new_ball.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
         balls += [new_ball]
@@ -114,9 +115,11 @@ class gun():
     def targetting(self, event=0):
         """Прицеливание. Зависит от положения мыши."""
         if event:
-            self.an = math.atan((event.y-450) / (event.x-20))
+            self.an = math.atan((event.y - 450) / (event.x - 20))
+
         if self.f2_on:
             canv.itemconfig(self.id, fill='orange')
+
         else:
             canv.itemconfig(self.id, fill='black')
         canv.coords(self.id, 20, 450,
@@ -128,7 +131,9 @@ class gun():
         if self.f2_on:
             if self.f2_power < 100:
                 self.f2_power += 1
+
             canv.itemconfig(self.id, fill='orange')
+
         else:
             canv.itemconfig(self.id, fill='black')
 
@@ -137,10 +142,9 @@ class target():
     def __init__(self):
         self.points = 0
         self.live = 1
-        self.id = canv.create_oval(0,0,0,0)
-        self.id_points = canv.create_text(30,30,text = self.points,font = '28')
+        self.id = canv.create_oval(0, 0, 0, 0)
+        self.id_points = canv.create_text(30, 30, text=self.points, font='28')
         self.new_target()
-    
 
     def new_target(self):
         """ Инициализация новой цели. """
@@ -148,7 +152,7 @@ class target():
         y = self.y = rnd(300, 550)
         r = self.r = rnd(2, 50)
         color = self.color = 'red'
-        canv.coords(self.id, x-r, y-r, x+r, y+r)
+        canv.coords(self.id, x - r, y - r, x + r, y + r)
         canv.itemconfig(self.id, fill=color)
 
     def hit(self, points=1):
@@ -160,13 +164,13 @@ class target():
 
 t1 = target()
 screen1 = canv.create_text(400, 300, text='', font='28')
-g1 = gun()
+g1 = Gun()
 bullet = 0
 balls = []
 
 
 def new_game(event=''):
-    global gun, t1, screen1, balls, bullet
+    global Gun, t1, screen1, balls, bullet
     t1.new_target()
     bullet = 0
     balls = []
@@ -176,27 +180,36 @@ def new_game(event=''):
 
     z = 0.03
     t1.live = 1
+
     while t1.live or balls:
         for b in balls:
             b.move()
+
             if b.hittest(t1) and t1.live:
                 t1.live = 0
                 t1.hit()
                 canv.bind('<Button-1>', '')
                 canv.bind('<ButtonRelease-1>', '')
-                canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
+                canv.itemconfig(
+                    screen1,
+                    text='Вы уничтожили цель за ' +
+                    str(bullet) +
+                    ' выстрелов')
+
             if b.live == 0:
                 canv.delete(b.id)
                 balls.remove(b)
+
         canv.update()
         time.sleep(0.03)
         g1.targetting()
         g1.power_up()
+
     canv.itemconfig(screen1, text='')
-    canv.delete(gun)
+    canv.delete(Gun)
     root.after(750, new_game)
 
 
 new_game()
 
-mainloop()
+tk.mainloop()
